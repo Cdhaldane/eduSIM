4343e452a64c3b2d19f9d704dd4958506b45d490
Christopher Venczel <christopher.venczel@outlook.com>
2021-09-13 17:41:00 -0400

    Starting to work on making context menu disappear when clicking away to remove the Close button.

--- client/src/components/ContextMenu/ContextMenu.jsx
+++ client/src/components/ContextMenu/ContextMenu.jsx
@@ -1,17 +1,33 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Dropdownedit from "../DropDown/Dropdownedit";
 import "./ContextMenu.css"
 
+function ContextMenu(props) {
+  console.log(props);
 
-function ContextMenu(props){
-  console.log(props)
   const [drop, setDrop] = useState(false);
+  const [clickedOutside, setClickedOutside] = React.useState(false);
+  const myRef = React.useRef();
 
-  function handleColorF(e){
+  const handleClickOutside = e => {
+    if (!myRef.current.contains(e.target)) {
+      setClickedOutside(true);
+    }
+  };
+
+  const handleClickInside = () => setClickedOutside(false);
+
+  // https://stackoverflow.com/a/50558760 For tommorrow!
+  React.useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  });
+
+  function handleColorF(e) {
     props.choosecolorf(e);
   }
 
-  function handleColorS(e){
+  function handleColorS(e) {
     props.choosecolors(e);
   }
 
@@ -19,49 +35,45 @@ function ContextMenu(props){
     setDrop(!drop);
   }
 
-  function handleWidth(e){
+  function handleWidth(e) {
     props.handleWidth(e);
   }
 
-  function handleOpacity(e){
+  function handleOpacity(e) {
     props.handleOpacity(e);
   }
 
-
-
-
   return (
-
     <div
+      ref={myRef} 
+      onClick={handleClickInside}
       className="cmenu"
       style={{
         position: "absolute",
-        left: props.position.x+100,
-        top: props.position.y-20,
+        left: props.position.x + 100,
+        top: props.position.y - 20,
       }}
     >
       <ul>
-        <li onClick={props.cut}>Cut</li>
-        <li onClick={props.copy}>Copy</li>
+        <li onClick={props.cut}>Cut1</li>
+        <li onClick={props.copy}>Copy2</li>
         <li onClick={props.paste}>Paste</li>
         <li onClick={props.delete}>Delete</li>
         <li onClick={handleEdit}>Edit shape</li>
-      <hr />
-    <li onClick={props.close}>Close</li>
+        <hr />
+        <li onClick={props.close}>Close</li>
       </ul>
-
 
       {drop && <div className="drop">
         <Dropdownedit
-            title="Edit Shape"
-            choosecolorf={handleColorF}
-            choosecolors={handleColorS}
-            handleWidth={handleWidth}
-            handleOpacity={handleOpacity}
-          />
+          title="Edit Shape"
+          choosecolorf={handleColorF}
+          choosecolors={handleColorS}
+          handleWidth={handleWidth}
+          handleOpacity={handleOpacity}
+        />
       </div>}
     </div>
-
   );
 };
 
--- client/src/components/DropDown/Dropdownroles.jsx
+++ client/src/components/DropDown/Dropdownroles.jsx
@@ -4,66 +4,62 @@ import axios from "axios"
 
 import "./Dropdown.css";
 
-  function Dropdowninfo(props) {
-    const [activeMenu, setActiveMenu] = useState('main');
-    const [menuHeight, setMenuHeight] = useState(null);
-    const dropdownRef = useRef(null);
-    const [rolename, setRolename] = useState("")
-    const [selectedRole, setSelectedRole] = useState("Select Role!");
-    const [roles, setRoles] = useState([])
+function Dropdowninfo(props) {
+  const [activeMenu, setActiveMenu] = useState('main');
+  const [menuHeight, setMenuHeight] = useState(null);
+  const dropdownRef = useRef(null);
+  const [rolename, setRolename] = useState("");
+  const [selectedRole, setSelectedRole] = useState("Select Role!");
+  const [roles, setRoles] = useState([]);
 
   useEffect(() => {
-    setMenuHeight(dropdownRef.current?.firstChild.scrollHeight)
-    console.log(props)
-    axios.get(process.env.REACT_APP_API_ORIGIN+'/api/gameroles/getGameRoles/:gameinstanceid', {
+    setMenuHeight(dropdownRef.current?.firstChild.scrollHeight);
+    axios.get(process.env.REACT_APP_API_ORIGIN + '/api/gameroles/getGameRoles/:gameinstanceid', {
       params: {
-            gameinstanceid: props.gameid,
-        }
-    })
-    .then((res) => {
+        gameinstanceid: props.gameid,
+      }
+    }).then((res) => {
       const allData = res.data;
       const stuff = [];
       for (let i = 0; i < allData.length; i++) {
         stuff.push({
-            gameroleid: res.data[i].gameroleid,
-            index: i,
-            role: res.data[i].gamerole,
+          gameroleid: res.data[i].gameroleid,
+          index: i,
+          role: res.data[i].gamerole,
         });
-      console.log(stuff)
-      setRoles(stuff);
-    }
-      // setRoles(res.data)
-    })
-    .catch(error => console.log(error.response));
+        setRoles(stuff);
+      }
+    }).catch(error => {
+      console.log(error);
+    });
   }, [props])
 
-  function updateGameRoles(){
-    axios.get(process.env.REACT_APP_API_ORIGIN+'/api/gameroles/getGameRoles/:gameinstanceid', {
+  function updateGameRoles() {
+    axios.get(process.env.REACT_APP_API_ORIGIN + '/api/gameroles/getGameRoles/:gameinstanceid', {
       params: {
-            gameinstanceid: props.gameid,
-        }
-    })
-    .then((res) => {
+        gameinstanceid: props.gameid,
+      }
+    }).then((res) => {
       const allData = res.data;
       const stuff = [];
       for (let i = 0; i < allData.length; i++) {
         stuff.push({
-            gameroleid: res.data[i].gameroleid,
-            index: i,
-            role: res.data[i].gamerole,
+          gameroleid: res.data[i].gameroleid,
+          index: i,
+          role: res.data[i].gamerole,
         });
-      console.log(stuff)
-      setRoles(stuff);
-      }  // setRoles(res.data)
-    })
-    .catch(error => console.log(error.response));
+        setRoles(stuff);
+      }
+    }).catch(error => {
+      console.log(error);
+    });
   }
-
 
   function calcHeight(el) {
     const height = el.offsetHeight;
     setMenuHeight(height);
   }
+
   function DropdownItem(props) {
     return (
       <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
@@ -82,60 +78,60 @@ import "./Dropdown.css";
       </a>
     );
   }
-  function handleRole(e){
-    console.log(e)
-    setSelectedRole(e)
-    props.roleLevel(e)
+
+  function handleRole(e) {
+    setSelectedRole(e);
+    props.roleLevel(e);
   }
-  function handleAddRole(){
-    console.log(rolename)
+
+  function handleAddRole() {
     let data = {
       gameinstanceid: props.gameid,
       gamerole: rolename
-    }
-      axios.post(process.env.REACT_APP_API_ORIGIN+'/api/gameroles/createRole', data)
-         .then((res) => {
-            console.log(res)
-            updateGameRoles();
-           })
-          .catch(error => console.log(error.response));
+    };
 
-  }
-  function handleRoleChange(e){
-    setRolename(e.target.value)
+    axios.post(process.env.REACT_APP_API_ORIGIN + '/api/gameroles/createRole', data).then((res) => {
+      updateGameRoles();
+    }).catch(error => {
+      console.log(error);
+    });
   }
 
-  function handleDelete(e){
-    axios.delete(process.env.REACT_APP_API_ORIGIN+'/api/gameroles/deleteRole/:gameroleid', {
+  function handleRoleChange(e) {
+    setRolename(e.target.value);
+  }
+
+  function handleDelete(e) {
+    axios.delete(process.env.REACT_APP_API_ORIGIN + '/api/gameroles/deleteRole/:gameroleid', {
       params: {
         id: roles[e].gameroleid
       }
-    })
-       .then((res) => {
-          console.log(res)
-          setSelectedRole("Select Role!")
-          props.roleLevel("Select Role!")
-         })
-        .catch(error => console.log(error.response));
-    console.log(e)
-    delete roles[e]
+    }).then((res) => {
+      console.log(res)
+      setSelectedRole("Select Role!")
+      props.roleLevel("Select Role!")
+    }).catch(error => {
+      console.log(error);
+    });
+
+    delete roles[e];
   }
 
- return (
-     <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
-       <CSSTransition
+  return (
+    <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
+      <CSSTransition
         in={activeMenu === 'main'}
         timeout={500}
         classNames="menu-primary"
         unmountOnExit
         onEnter={calcHeight}>
         <div className="menu">
-            <DropdownItem
-              leftIcon={<i id="icons" class="fab fa-critical-role"></i>}
-              rightIcon={""}
-              goToMenu="roles">
-              {selectedRole}
-            </DropdownItem>
+          <DropdownItem
+            leftIcon={<i id="icons" class="fab fa-critical-role"></i>}
+            rightIcon={""}
+            goToMenu="roles">
+            {selectedRole}
+          </DropdownItem>
         </div>
       </CSSTransition>
 
@@ -150,7 +146,7 @@ import "./Dropdown.css";
             <h2>{selectedRole}</h2>
           </DropdownItem>
           {roles.map((index) => {
-            return(
+            return (
               <DropdownItems onClack={() => handleRole(index.role)} value={index.index} leftIcon={<i id="icons" class="fa fa-trash" onClick={() => handleDelete(index.index)}></i>}>{index.role}</DropdownItems>
             )
           })}
@@ -161,25 +157,25 @@ import "./Dropdown.css";
       </CSSTransition>
 
       <CSSTransition
-       in={activeMenu === 'addrole'}
-       timeout={500}
-       classNames="menu-primary"
-       unmountOnExit
-       onEnter={calcHeight}>
-       <div className="menu">
-         <DropdownItem goToMenu="roles" leftIcon={<i id="icons" class="fas fa-arrow-left"></i>}>
-           <h2>ROLES!</h2>
-         </DropdownItem>
-         <DropdownItem
-           leftIcon={<i id="icons" class="fas fa-plus"
-           onClick={handleAddRole}
-           ></i>}
-           goToMenu="main">
+        in={activeMenu === 'addrole'}
+        timeout={500}
+        classNames="menu-primary"
+        unmountOnExit
+        onEnter={calcHeight}>
+        <div className="menu">
+          <DropdownItem goToMenu="roles" leftIcon={<i id="icons" class="fas fa-arrow-left"></i>}>
+            <h2>ROLES!</h2>
+          </DropdownItem>
+          <DropdownItem
+            leftIcon={<i id="icons" class="fas fa-plus"
+              onClick={handleAddRole}
+            ></i>}
+            goToMenu="main">
 
-       </DropdownItem>
-         <input id="rolenameinput" type="text" placeholder="Role Name!" onChange={handleRoleChange} value={rolename} />
-       </div>
-     </CSSTransition>
+          </DropdownItem>
+          <input id="rolenameinput" type="text" placeholder="Role Name!" onChange={handleRoleChange} value={rolename} />
+        </div>
+      </CSSTransition>
     </div>
   );
 }
