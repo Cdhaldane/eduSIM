9f2801d06cc415314a11d989c94c0b860a394c9b
Christopher Venczel <christopher.venczel@outlook.com>
2021-10-26 15:26:03 -0400

    Added info popup

--- client/src/components/Level/Level.jsx
+++ client/src/components/Level/Level.jsx
@@ -1,20 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { times } from "lodash";
 import { Link } from "react-router-dom";
-import Pencil from "../Pencils/Pencil"
+import Pencil from "../Pencils/Pencil";
+import Modal from "react-modal";
 
-import "./Level.css"
+import "./Level.css";
 
 const Level = (props) => {
+
   const [count, setCount] = useState(1);
+  const [showInfoPopup, setShowInfoPopup] = useState(false);
+  const infoPopup = new useRef();
+  const infoBtn = new useRef();
+
   let items = [];
+
+  const handleClickOutside = e => {
+    console.log(e.target);
+    if (infoPopup.current && infoBtn.current && !(
+      infoPopup.current.contains(e.target) ||
+      infoBtn.current.contains(e.target))) {
+      setShowInfoPopup(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+    return () => document.removeEventListener('click', handleClickOutside);
+  }, []);
 
   const handleLevel = (e) => {
     props.level(e);
   }
 
   const handleCount = () => {
-    // setCount(count + 1);
     handleLevel(count + 1);
   }
 
@@ -56,7 +75,110 @@ const Level = (props) => {
   return (
     <div id="levelContainer">
       <div className={`level ${props.gamepage ? 'level-gamepage' : ''}`}>
-        {!props.gamepage && <h1 id="editModeTitle">Edit Mode</h1>}
+        {!props.gamepage && (
+          <>
+            <h1 id="editModeTitle">
+              Edit Mode
+            </h1>
+            <button id="levelInfoButton" onClick={() => setShowInfoPopup(true)} ref={infoBtn}>
+              <i className="fa fa-info" />
+            </button>
+
+            <Modal
+              isOpen={showInfoPopup}
+              onRequestClose={() => setShowInfoPopup(false)}
+              className="createmodalarea"
+              overlayClassName="myoverlay"
+              closeTimeoutMS={250}
+              ariaHideApp={false}
+            >
+              <div className="area">
+                <form className="form-input" ref={infoPopup} style={{ padding: "20px" }}>
+                  <div className="levelInstructionsDiv">
+                    <h2>Instructions</h2>
+                    <table>
+                      <tbody className="levelInstructionsTable">
+                        <tr>
+                          <td style={{
+                            width: "50%",
+                            paddingRight: "30px"
+                          }}>
+                            <h3>Overview</h3>
+                            <p style={{
+                              textAlign: "left",
+                              paddingTop: "7px"
+                            }}>
+                              Edit Mode allows you to create and edit simulations.
+                              Simulations have two main components: the group area (with the grid background), and the personal area (located at the bottom of the screen).
+                              To toggle opening/closing the personal area use the red arrow button at the top right of the personal area.
+                              The group area will be shared by all the users in a simulation while the personal area contents are determined by the user's role and are only visible to the user.
+                              To create/edit roles use the role picker located in the top left of the personal area.
+                            </p>
+                          </td>
+                          <td style={{ width: "50%" }}>
+                            <h3>Shortcuts</h3>
+                            <ul>
+                              <li>
+                                Right click on canvas
+                                <i className="fa fa-angle-right" />
+                                Add Menu
+                              </li>
+                              <li>
+                                Right click on object
+                                <i className="fa fa-angle-right" />
+                                Object Menu
+                              </li>
+                              <li>
+                                Ctrl + Drag
+                                <i className="fa fa-angle-right" />
+                                Move around canvas
+                              </li>
+                              <li>
+                                Shift + R
+                                <i className="fa fa-angle-right" />
+                                Recenter Contents
+                              </li>
+                              <li>
+                                Delete
+                                <i className="fa fa-angle-right" />
+                                Delete selected object
+                              </li>
+                              <li>
+                                Ctrl + X
+                                <i className="fa fa-angle-right" />
+                                Cut selected object
+                              </li>
+                              <li>
+                                Ctrl + C
+                                <i className="fa fa-angle-right" />
+                                Copy selected object
+                              </li>
+                              <li>
+                                Ctrl + V
+                                <i className="fa fa-angle-right" />
+                                Paste selected object
+                              </li>
+                              <li>
+                                Ctrl + Z
+                                <i className="fa fa-angle-right" />
+                                Undo action
+                              </li>
+                              <li>
+                                Ctrl + Shift + Z
+                                <i className="fa fa-angle-right" />
+                                Redo action
+                              </li>
+                            </ul>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </form>
+              </div>
+            </Modal>
+          </>
+        )}
         <div className="level-nav">
           {!props.gamepage && (
             <select className="level-select" onChange={handleChange} value={count}>
