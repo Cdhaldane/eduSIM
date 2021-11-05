0d71236a096098a9588c71105655f9b390e25b40
Christopher Venczel <christopher.venczel@outlook.com>
2021-11-05 12:20:21 -0400

    More CSS changes to the performance tab. Starting to work on saviong performance info to state

--- client/src/components/SideBar/Performance.jsx
+++ client/src/components/SideBar/Performance.jsx
@@ -1,14 +1,23 @@
 import React, { useState, forwardRef } from "react";
+import Switch from "react-switch";
 
 import "./Performance.css";
 
 const Performance = forwardRef((props, ref) => {
 
   const [customObjs, setCustomObjs] = useState(props.customObjs);
+  const [pollChecks, setPollChecks] = useState(props.customObjs.polls ? Array(props.customObjs.polls.length).fill(false) : []);
 
-  console.log(customObjs);
+  const savePerformanceInfo = () => {
+    const json = {
+      polls: null
+    }
 
-  console.log(Object.keys(customObjs).some(key => customObjs[key].length));
+    // Save poll info
+    for (let i = 0; i < customObjs.polls.length; i++) {
+      const poll = customObjs.polls[i].json;
+    }
+  }
 
   return (
     <div className="area">
@@ -25,26 +34,56 @@ const Performance = forwardRef((props, ref) => {
                         return (
                           <tr key={i} className="performancePollRow">
                             <td>
+                              <Switch
+                                height={30}
+                                width={50}
+                                onChange={(val) => {
+                                  const newChecks = [...pollChecks];
+                                  newChecks[i] = val;
+                                  setPollChecks(newChecks);
+                                }}
+                                checked={pollChecks[i]}
+                                className="react-switch"
+                              />
                               <span>
                                 {`Poll ${i + 1} - ${poll.customName ? poll.customName : "Untitled"}`}
                               </span>
-                              {poll.json.pages.map((page, pageI) => (
-                                <React.Fragment key={pageI}>
-                                  <h3>
-                                    Page #{pageI + 1}
-                                  </h3>
-                                  {page.questions.map((q, qI) => (
-                                    <div key={qI}>
-                                      <div className="performanceQNames">
-                                        {q.title}
-                                      </div>
-                                      <div className="performanceQOptions">
-                                        Test
-                                      </div>
-                                    </div>
+                              {pollChecks[i] && (
+                                <>
+                                  {poll.json.pages.map((page, pageI) => (
+                                    <React.Fragment key={pageI}>
+                                      <h3>
+                                        Page #{pageI + 1}
+                                      </h3>
+                                      {page.questions.map((q, qI) => (
+                                        <div key={qI}>
+                                          <div className="performanceQNames">
+                                            {q.title}
+                                          </div>
+                                          <div className="performanceQOptions">
+                                            <div>
+                                              <input defaultChecked type="radio" name={`poll${i}Q${qI}`} value="allAnswers" />
+                                              <label>List All User Answers</label>
+                                            </div>
+                                            <div>
+                                              <input type="radio" name={`poll${i}Q${qI}`} value="mostCommon" />
+                                              <label>Show Most Common Answer</label>
+                                            </div>
+                                            <div>
+                                              <input type="radio" name={`poll${i}Q${qI}`} value="usersOwn" />
+                                              <label>Show Only User's Own Answer</label>
+                                            </div>
+                                            <div>
+                                              <input type="radio" name={`poll${i}Q${qI}`} value="doNotShow" />
+                                              <label>Do Not Show This Question</label>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      ))}
+                                    </React.Fragment>
                                   ))}
-                                </React.Fragment>
-                              ))}
+                                </>
+                              )}
                             </td>
                           </tr>
                         );
