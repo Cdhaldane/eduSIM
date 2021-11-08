09ecfbdd539205a2a8febd32a79d0ffe94561ace
Christopher Venczel <christopher.venczel@outlook.com>
2021-11-08 17:35:02 -0500

    Poll now synchronized in group area.

--- client/src/components/Stage/GamePieces/Poll/Poll.jsx
+++ client/src/components/Stage/GamePieces/Poll/Poll.jsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useEffect } from 'react';
+import React, { forwardRef, useEffect, useState } from 'react';
 import * as Survey from "survey-react";
 import CustomWrapper from "../CustomWrapper";
 
@@ -7,7 +7,65 @@ import "survey-react/survey.css";
 
 const Poll = forwardRef((props, ref) => {
 
-  const onComplete = (survey, options) => {
+  const [survey, setSurvey] = useState(new Survey.Model(props.defaultProps.custom.pollJson));
+
+  useEffect(() => {
+    setSurvey(new Survey.Model(props.defaultProps.custom.pollJson));
+  }, [props.defaultProps.custom.pollJson]);
+
+  useEffect(() => {
+    // Synchronize the poll answers, page, and completion for all users
+    // by reading from the saved synched status variable
+    if (props.status.data) {
+      for (const q in props.status.data) {
+        const answer = props.status.data[q];
+        survey.setValue(q, answer);
+      }
+    }
+
+    if (props.status.page) {
+      if (props.status.page === "next") {
+        survey.nextPage();
+      } else {
+        survey.prevPage();
+      }
+      props.updateStatus({
+        ...props.status,
+        page: null,
+      });
+    }
+
+    if (props.status.isComplete) {
+      survey.doComplete();
+    }
+  }, [props.status]);
+
+  const onValueChanged = (survey) => {
+    props.updateStatus({
+      ...props.status,
+      data: survey.data
+    });
+  }
+
+  const onCurrentPageChanged = (survey, options) => {
+    let pageStatus = null;
+    if (options.isNextPage) {
+      pageStatus = "next";
+    } else if (options.isPrevPage) {
+      pageStatus = "prev";
+    }
+    props.updateStatus({
+      ...props.status,
+      page: pageStatus,
+    });
+  }
+
+  const onComplete = (survey) => {
+    props.updateStatus({
+      ...props.status,
+      isComplete: true
+    });
+
     // Save survey results
     /*props.updateStatus({
       data: survey.data
@@ -15,31 +73,14 @@ const Poll = forwardRef((props, ref) => {
     //props.defaultProps.onComplete(survey.data);
   }
 
-  useEffect(() => {
-    console.log(props.status);
-    for (q in props.status) {
-      console.log(q);
-    }
-  }, [props.status]);
-
-  const onValueChanged = (survey) => {
-    props.updateStatus({
-      data: survey.data
-    });
-    //console.log(survey.data);
-    //survey.setValue("Page 1 Question 1", "OOGA");
-    // props.updateStatus({
-    //   data: survey.data
-    // });
-  }
-
   return (
     <CustomWrapper {...props} ref={ref}>
       <div className="poll">
         <Survey.Survey
+          model={survey}
           onValueChanged={onValueChanged}
+          onCurrentPageChanged={onCurrentPageChanged}
           onComplete={onComplete}
-          json={props.defaultProps.custom.pollJson}
         />
       </div>
     </CustomWrapper>
