81e70d031b70232060fee46513dd25a72dfe3700
Morris Raycroft <electrica5000@gmail.com>
2021-12-01 10:06:58 -0500

    player submenu style

--- client/src/components/SideBar/submenus/Players.jsx
+++ client/src/components/SideBar/submenus/Players.jsx
@@ -9,6 +9,30 @@ const Container = styled.div`
   padding: 20px;
 `;
 
+const Player = styled.div`   
+  display: flex;
+  align-items: center;
+  margin-bottom: 5px;
+  & > p { 
+    width: 40px;
+    height: 40px;
+    background-color: hsl(${p => p.hue || 0}, 66%, 40%);
+    color: white;
+    font-size: 1.5em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 999px;
+    margin-right: 10px;
+  }
+  & > div > b {
+    font-size: .9em;
+  }
+  & > div > p {
+    font-size: .8em;
+  }
+`;
+
 function Players({ players }) {
 
   return (
@@ -16,8 +40,14 @@ function Players({ players }) {
       <h2>Players</h2>
       <hr/>
       <div>
-        {Object.values(players || {}).map(({name, role}) => (
-          <p>{name} ({role})</p>
+        {Object.values(players || {}).map(({dbid, name, role}) => (
+          <Player hue={parseInt(dbid, 16) % 360}>
+            <p>{name[0]}</p>
+            <div>
+              <b>{name}</b>
+              <p>{role}</p>
+            </div>
+          </Player>
         ))}
       </div>
     </Container>
