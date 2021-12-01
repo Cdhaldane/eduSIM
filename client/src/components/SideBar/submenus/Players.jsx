1284ea9214143c8e22f59ebe8d0920b6270152b9
Morris Raycroft <electrica5000@gmail.com>
2021-12-01 12:43:00 -0500

    "you" indicator in player submenu

--- client/src/components/SideBar/submenus/Players.jsx
+++ client/src/components/SideBar/submenus/Players.jsx
@@ -12,13 +12,16 @@ const Container = styled.div`
 const Player = styled.div`   
   display: flex;
   align-items: center;
-  margin-bottom: 5px;
+  margin-bottom: 2px;
+  padding: 6px;
+  border-radius: 6px;
+  ${p => p.me && 'background-color: #a4db7f59;'}
   & > p { 
-    width: 40px;
-    height: 40px;
+    width: 34px;
+    height: 34px;
     background-color: hsl(${p => p.hue || 0}, 66%, 40%);
     color: white;
-    font-size: 1.5em;
+    font-size: 1.3em;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -28,6 +31,9 @@ const Player = styled.div`
   & > div > b {
     font-size: .9em;
   }
+  & > div > b > span {
+    color: #3e7d14;
+  }
   & > div > p {
     font-size: .8em;
   }
@@ -35,16 +41,18 @@ const Player = styled.div`
 
 function Players({ players }) {
 
+  const userDBID = JSON.parse(localStorage.userInfo || '{}')?.dbid;
+
   return (
     <Container>
       <h2>Players</h2>
       <hr/>
       <div>
         {Object.values(players || {}).map(({dbid, name, role}) => (
-          <Player hue={parseInt(dbid, 16) % 360}>
+          <Player hue={parseInt(dbid, 16) % 360} me={userDBID === dbid}>
             <p>{name[0]}</p>
             <div>
-              <b>{name}</b>
+              <b>{name} {userDBID === dbid && <span>(you)</span>}</b>
               <p>{role}</p>
             </div>
           </Player>
