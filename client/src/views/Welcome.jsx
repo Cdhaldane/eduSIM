389798335d37b0c859abc02c1b43256b58f0c2dc
Morris Raycroft <electrica5000@gmail.com>
2021-12-23 10:37:42 -0500

    join using student welcome page, translation fixes

--- client/src/views/Welcome.jsx
+++ client/src/views/Welcome.jsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth0 } from '@auth0/auth0-react';
 import { useTranslation, Trans } from "react-i18next";
+import axios from "axios";
+import { useAlertContext } from "../components/Alerts/AlertContext";
+import { useHistory } from "react-router-dom";
 
 const Welcome = (props) => {
   const { t } = useTranslation();
   const { loginWithRedirect } = useAuth0();
+  const alertContext = useAlertContext();
+  let history = useHistory();
+  const [code, setCode] = useState("");
+
+  const joinGame = async (e) => {
+    e.preventDefault();
+    
+    const { data: roomData } = await axios.get(
+      process.env.REACT_APP_API_ORIGIN + '/api/playerrecords/getRoomByURL', {
+      params: {
+        id: code,
+      }
+    });
+
+    if (!!roomData) {
+      history.push(`/gamepage/${code}`);
+    } else {
+      alertContext.showAlert(t("alert.roomDoesntExist"), "error");
+    }
+
+    return false;
+  };
 
   return (
     <div className="welcome-container">
       <h1 className="welcome welcome-noanim">{t("home.welcome")}</h1>
       <p>{t("home.alreadyHaveRoomCode")}</p>
-      <p>
-        <form action={window.location.origin + document.getElementById.value}>
-          <label className="welcome-join">
-            {t("home.join")}
-            <input type="text" className="textbox" placeholder={t("home.roomCode")} id="code" />
-          </label>
-        </form>
-      </p>
+      <form onSubmit={joinGame} className="welcome-join">
+        <input type="text" className="textbox" placeholder={t("home.roomCode")} value={code} onChange={(e) => setCode(e.target.value)} />
+        <input type="submit" value={t("home.join")} />
+      </form>
       <p>
         <Trans i18nKey="home.getCodeOrCreateSim">
           If not, get one from your teacher/facilitator, or 
