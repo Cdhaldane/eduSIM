1894a6e938d3286c790510c00dfa86594faf8f8d
cdhaldane <48765995+Cdhaldane@users.noreply.github.com>
2022-01-27 20:06:20 -0500

    test

--- client/src/components/EditableRow.jsx
+++ client/src/components/EditableRow.jsx
@@ -14,7 +14,7 @@ const EditableRow = ({
   groupList
 }) => {
   const { t } = useTranslation();
-  
+
   return (
     <tr className={online ? "user-online" : ""}>
       {onCheck && (
@@ -54,22 +54,22 @@ const EditableRow = ({
       </td>
       <td>
         {useGroup ? (
-          <select name="group" type="text" required="required" id="roledropdown" onChange={handleEditFormChange}>
+          <select name="group" type="text" required="required" id="roledropdownadd" onChange={handleEditFormChange}>
             <option key={-1} value="">{t("admin.selectAGroup")}</option>)];
             {groupList.map(val => (
               <option key={val[1]} value={val[0]}>{val[0]}</option>
             ))}
           </select>
         ) : (
-          <select name="gamerole" type="text" required="required" id="roledropdown" onChange={handleEditFormChange}>
+          <select name="gamerole" type="text" required="required" id="roledropdownadd" onChange={handleEditFormChange}>
             {rolelist}
           </select>
         )}
       </td>
       <td>
         <div>
-          <button type="submit" >{t("common.save")}</button>
-          <button type="button" onClick={handleCancelClick}>
+          <button className="modal-button green" type="submit" >{t("common.save")}</button>
+          <button className="modal-button red" type="button" onClick={handleCancelClick}>
           {t("common.cancel")}
           </button>
         </div>
