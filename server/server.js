425d9b50712ad41bf30c22540d18e100b91aef56
cdhaldane <48765995+Cdhaldane@users.noreply.github.com>
2023-08-17 16:11:46 -0400

    production

--- server/server.js
+++ server/server.js
@@ -1,34 +1,33 @@
-require('dotenv').config()
+require('dotenv').config();
 import helmet from 'helmet';
 
 import routes from './routes';
 import events from './events';
-import clean from './routes/dbCleanup'
-const fs = require("fs");
-const https = require("https");
-const http = require("http");
+import clean from './routes/dbCleanup';
+
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
 
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 
+let cors = require('cors');
 
-let cors = require('cors')
-
-const ENV = process.env.NODE_ENV  //Tells us if we're working in development or production
+const ENV = process.env.NODE_ENV; // Tells us if we're working in development or production
 const PORT = process.env.PORT || 8080;
 
-
-//Initialising express and registering the basic middleware
+// Initializing express and registering the basic middleware
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://edusim.ca', "https://reactapp-jyfluau3gq-uc.a.run.app", "https://react-frontend-socurjfina-uc.a.run.app", "https://hgjdchierhxsyosxrfqi.supabase.co"],
+  origin: ['http://localhost:3000', 'https://edusim.ca', 'https://reactapp-jyfluau3gq-uc.a.run.app', 'https://react-frontend-socurjfina-uc.a.run.app', 'https://hgjdchierhxsyosxrfqi.supabase.co'],
   optionsSuccessStatus: 200,
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-}
+};
 
 app.use(cors(corsOptions));
 app.use(helmet());
@@ -52,101 +51,46 @@ app.use('/api/video', routes.video);
 app.use((req, res) => {
   res.status(404).send('404: Page not found');
 });
-let io;
+
+let server;
+
 if (ENV === 'production') {
+  // Use HTTPS in production
+  const privateKey = fs.readFileSync('/etc/letsencrypt/live/edusim.ca/privkey.pem', 'utf8');
+  const certificate = fs.readFileSync('/etc/letsencrypt/live/edusim.ca/fullchain.pem', 'utf8');
 
-  app.enable("trust proxy");
-  app.use((req, res, next) => {
-    console.log("https://" + req.headers.host + req.url);
-    console.log(req.secure);
-    req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
-  });
-
-  const privateKey = fs.readFileSync(
-    path.join(__dirname, `./privkey.pem`),
-    "utf8"
-  );
-  const certificate = fs.readFileSync(
-    path.join(__dirname, `./fullchain.pem`),
-    "utf8"
-  );
-
-  const httpsServer = https.createServer(
-    {
-      key: privateKey,
-      cert: certificate,
-    },
-    app
-  );
-
-  io = require("socket.io")(httpsServer, {
-    cors: {
-      origin: true,
-      credentials: true
-    },
-  });
+  server = https.createServer({ key: privateKey, cert: certificate }, app);
 } else {
-  const httpServer = https.createServer(app);
-  io = require("socket.io")(httpServer, {
-    cors: {
-      origin: true,
-      credentials: true
-    },
-  });
+  // Use HTTP otherwise
+  server = http.createServer(app);
 }
 
-io.on("connection", (socket) => events(io, socket));
+const io = require('socket.io')(server, {
+  cors: {
+    origin: true,
+    credentials: true,
+  },
+});
+
+io.on('connection', (socket) => events(io, socket));
 io.eio.pingTimeout = 320000; // 2 minutes
 io.eio.pingInterval = 10000;  // 5 seconds
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   socket.on('disconnect', function () {
     console.log('Got disconnect!');
   });
-  socket.on("disconnecting", (reason) => {
+  socket.on('disconnecting', (reason) => {
     for (const room of socket.rooms) {
       if (room !== socket.id) {
-        socket.to(room).emit("errorLog", { key: "alert.noUpdateGameXInProgress", params: { game: socket.id } });
+        socket.to(room).emit('errorLog', { key: 'alert.noUpdateGameXInProgress', params: { game: socket.id } });
       }
     }
   });
 });
 
-console.log(ENV)
-if (ENV === 'production') {
-  // Redirect HTTP requests to HTTPS
-
-  app.enable("trust proxy");
-  app.use((req, res, next) => {
-    console.log("https://" + req.headers.host + req.url);
-    console.log(req.secure);
-    req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
-  });
-
-  const privateKey = fs.readFileSync(
-    path.join(__dirname, `./privkey.pem`),
-    "utf8"
-  );
-  const certificate = fs.readFileSync(
-    path.join(__dirname, `./fullchain.pem`),
-    "utf8"
-  );
-
-  const httpsServer = https.createServer(
-    {
-      key: privateKey,
-      cert: certificate,
-    },
-    app
-  );
-
-  httpsServer.listen(PORT, () =>
-    console.log(`HTTPS server started on PORT=${PORT}`)
-  );
-}
-
-
-
-
+server.listen(PORT, () =>
+  console.log(`Server started on port ${PORT} in ${ENV} mode!`)
+);
 
 module.exports = app;
